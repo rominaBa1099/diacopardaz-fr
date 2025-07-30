734a3167a66064919358e655f966c98256e5d1a9
romina_ba <93289279+rominaBa1099@users.noreply.github.com>
2025-07-30 13:26:10 +0400

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,22 +4,11 @@ import "@/styles/date-picker.css";
 import { PlasmicRootProvider } from "@plasmicapp/react-web";
 import type { AppProps } from "next/app";
 import Head from "next/head";
-import { useEffect, useState } from "react";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  const [theme, setTheme] = useState<"light" | "dark">("dark");
-
-  useEffect(() => {
-    const urlTheme = new URLSearchParams(window.location.search).get("theme");
-    if (urlTheme === "light" || urlTheme === "dark") {
-      setTheme(urlTheme);
-      document.documentElement.setAttribute("data-theme", urlTheme);
-    }
-  }, []);
-
   return (
-    <PlasmicRootProvider Head={Head} globalContexts={{ Theme: theme }}>
+    <PlasmicRootProvider Head={Head}>
       <Component {...pageProps} />
     </PlasmicRootProvider>
   );
-}
+} 
