ac8469fb995c41c0cc395240ca231159e6f0dd0f
romina_ba <93289279+rominaBa1099@users.noreply.github.com>
2025-07-30 13:28:28 +0330

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,3 @@
-// pages/_app.tsx
 import "@/styles/globals.css";
 import "@/styles/date-picker.css";
 
@@ -7,6 +6,9 @@ import type { AppProps } from "next/app";
 import Head from "next/head";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
+
+// اینو اضافه کن، چون Plasmic این Provider رو ساخته
+import { ThemeContextProvider } from "@/components/plasmic/metoo/PlasmicGlobalVariant__Theme";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
@@ -21,11 +23,10 @@ export default function MyApp({ Component, pageProps }: AppProps) {
   }, [router.query]);
 
   return (
-    <PlasmicRootProvider
-      Head={Head}
-      globalVariants={[{ name: "Theme", value: theme }]}
-    >
-      <Component {...pageProps} />
+    <PlasmicRootProvider Head={Head}>
+      <ThemeContextProvider value={theme}>
+        <Component {...pageProps} />
+      </ThemeContextProvider>
     </PlasmicRootProvider>
   );
 }
