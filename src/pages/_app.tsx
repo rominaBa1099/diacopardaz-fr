0e59c6c9f9a467e7ae35dd3da46d7bfd3ffcb7ff
romina_ba <93289279+rominaBa1099@users.noreply.github.com>
2025-07-30 11:44:44 +0330

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,13 @@
 import "@/styles/globals.css";
 import "@/styles/date-picker.css";
 
-import { PlasmicRootProvider } from "@plasmicapp/react-web";
+import { PlasmicRootProvider, GlobalVariantProvider } from "@plasmicapp/react-web";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 import { useEffect, useState } from "react";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  const [theme, setTheme] = useState<"light" | "dark">("dark"); // مقدار پیش‌فرض
+  const [theme, setTheme] = useState<"light" | "dark">("dark");
 
   useEffect(() => {
     const urlTheme = new URLSearchParams(window.location.search).get("theme");
@@ -17,8 +17,10 @@ export default function MyApp({ Component, pageProps }: AppProps) {
   }, []);
 
   return (
-    <PlasmicRootProvider Head={Head} globalContexts={{ Theme: theme }}>
-      <Component {...pageProps} />
+    <PlasmicRootProvider Head={Head}>
+      <GlobalVariantProvider variants={{ Theme: theme }}>
+        <Component {...pageProps} />
+      </GlobalVariantProvider>
     </PlasmicRootProvider>
   );
 }
